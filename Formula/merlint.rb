e4fd725a4dfebb96fade5ca055f6f63c0afacf12
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-02-04 20:20:00 -0800

    Update bottles 2026-02-04

--- Formula/merlint.rb
+++ Formula/merlint.rb
@@ -1,22 +1,22 @@
-class Prune < Formula
-  desc "Find and remove unused exports in OCaml interface files"
-  homepage "https://tangled.org/gazagnaire.org/prune"
+class Merlint < Formula
+  desc "Opinionated OCaml linter powered by Merlin"
+  homepage "https://tangled.org/gazagnaire.org/merlint"
   license "ISC"
   version "20260204"
 
   on_macos do
     on_arm do
-      url "https://homebrew-bottles.s3.fr-par.scw.cloud/prune-20260204.arm64_sonoma.bottle.tar.gz"
+      url "https://homebrew-bottles.s3.fr-par.scw.cloud/merlint-20260204.arm64_sonoma.bottle.tar.gz"
       sha256 :no_check
     end
     on_intel do
-      url "https://homebrew-bottles.s3.fr-par.scw.cloud/prune-latest.sonoma.bottle.tar.gz"
+      url "https://homebrew-bottles.s3.fr-par.scw.cloud/merlint-latest.sonoma.bottle.tar.gz"
       sha256 :no_check
     end
   end
 
   on_linux do
-    url "https://homebrew-bottles.s3.fr-par.scw.cloud/prune-latest.x86_64_linux.bottle.tar.gz"
+    url "https://homebrew-bottles.s3.fr-par.scw.cloud/merlint-latest.x86_64_linux.bottle.tar.gz"
     sha256 :no_check
   end
 
@@ -33,14 +33,14 @@ class Prune < Formula
     if build.head?
       system "opam", "init", "--disable-sandboxing", "--no-setup", "-y" unless File.exist?("#{Dir.home}/.opam")
       system "opam", "install", ".", "--deps-only", "--with-test=false", "-y", "--working-dir"
-      system "opam", "exec", "--", "dune", "build", "prune/bin/prune.exe"
-      bin.install "_build/default/prune/bin/prune.exe" => "prune"
+      system "opam", "exec", "--", "dune", "build", "merlint/bin/main.exe"
+      bin.install "_build/default/merlint/bin/main.exe" => "merlint"
     else
-      bin.install "prune"
+      bin.install "merlint"
     end
   end
 
   test do
-    system bin/"prune", "--help"
+    system bin/"merlint", "--help"
   end
 end
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ brew tap gazagnaire.org/monopam https://tangled.org/gazagnaire.org/homebrew-mono
 
 | Formula | Description |
 |---------|-------------|
+| `merlint` | Opinionated OCaml linter powered by Merlin |
 | `prune` | Find and remove unused exports in OCaml interface files |
 | `irmin` | Content-addressable store with Git and ATProto MST support |
 | `crow` | Crowbar campaign orchestrator for AFL fuzzing |
@@ -23,7 +24,7 @@ brew tap gazagnaire.org/monopam https://tangled.org/gazagnaire.org/homebrew-mono
 
 ```bash
 # Install pre-built binary (fast)
-brew install prune
+brew install merlint prune
 brew install irmin crow agent mdns-query uniboot
 
 # Or build from source
